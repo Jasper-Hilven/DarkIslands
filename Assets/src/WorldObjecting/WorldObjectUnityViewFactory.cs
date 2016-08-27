9fcb5bfac9068e8033db5ab7f4a0c1a6d0ea3ddf
Jasper Hilven <jasperhilven@gmail.com>
2016-08-27 15:19:52 +0200

    Found some trees

--- Assets/src/WorldObjecting/WorldObjectUnityViewFactory.cs
+++ Assets/src/WorldObjecting/WorldObjectUnityViewFactory.cs
@@ -4,10 +4,14 @@ namespace DarkIslands
 {
     public partial class WorldObjectUnityViewFactory
     {
+        private System.Random r = new System.Random();
         private GameObjectManager gB = new GameObjectManager();
         public GameObject GetTreeVisualization(WorldObject worldObject)
         {
-            var gO = gB.LoadViaResources("Tree");
+            var side = r.Next(2) > 0 ? "A" : "B";
+            var treeNb = 1+r.Next(5);
+            var gO = gB.LoadViaResources("Tree0"+treeNb+side);
+            gO.transform.Rotate(new Vector3(0,0,1),r.Next(360));
             this.ModelToEntity.modelToEntity.Add(gO, worldObject);
             return gO;
         }
